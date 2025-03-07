344a736dfd17748accd68a878ebfd35f49f54b3d
Dmitry Ivanov <ivanovdmitry91@gmail.com>
2025-03-07 03:10:37 +0300

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -8735,7 +8735,10 @@ console.log('end ovm install ');
     value.push('ovm which current');
  value.push('if [ ! -f /Users/runner/.local/share/ovm/current/lib/opm ]; then');
  value.push('echo "File not found!"');	    
- value.push('fi');
+ value.push('fi');  
+ value.push('cd  /Users/runner/.local/share/ovm/current/lib/');
+ value.push('ls');
+	   
  value.push('/Users/runner/.local/share/ovm/current/lib/opm');
  value.push('opm');	
 	    
